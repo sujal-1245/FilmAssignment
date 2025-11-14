1685eeb435e41cc8e828f3a5e6c2156502018a30
sujal-1245 <sujaldbhagat2004@gmail.com>
2025-11-14 10:19:41 +0530

    expanded tabs hover error fixed

--- frontend/src/components/ui/expanded-tabs.tsx
+++ frontend/src/components/ui/expanded-tabs.tsx
@@ -98,7 +98,7 @@ export function ExpandedTabs({
             initial="initial"
             animate="animate"
             custom={isHovered}
-            whileHover={() => setHovered(index)}
+            onHoverStart={() => setHovered(index)}
             onHoverEnd={() => setHovered(null)}
             onClick={() => handleClick(index, tab)}
             transition={transition}
